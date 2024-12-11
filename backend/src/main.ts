0f01d8a0355b57e977fba24fd9def9e43db8ff14
Tharindu Dan <mahathun.online@gmail.com>
2024-12-12 03:25:28 +1300

    feat(api): add cors settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as experss from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:5173'
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();

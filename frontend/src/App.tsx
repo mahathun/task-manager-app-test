9cacb4276d8d93e65d170fc09aebfcf1705feb22
Tharindu Dan <mahathun.online@gmail.com>
2024-12-12 03:26:38 +1300

    feat(frontend): install taliwind and shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import { fetchTasks } from './api/APITasks'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])  
+
+  const onClickHandler = async () => {  
+
+    const data = await fetchTasks()
+    console.log("data",data)
+    setTasks(data)
+
+  }
 
   return (
     <>
@@ -28,6 +38,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <button className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" onClick={onClickHandler}>
+      Click me
+    </button>
     </>
   )
 }

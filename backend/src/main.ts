2e524f9de0be8a56eccc1221dceede03cccb4f58
Tharindu Dan <mahathun.online@gmail.com>
2024-12-12 14:51:01 +1300

    feat(cors): add cors settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as experss from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173'
+    origin: ['http://localhost:5173', 'https://task-manager-app-test.vercel.app']
   });
   await app.listen(process.env.PORT ?? 4000);
 }
